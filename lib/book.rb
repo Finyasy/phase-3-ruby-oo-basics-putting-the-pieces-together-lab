120875e2159f9d6ac468b932368328c444faa1ae
Bryan Bosire <bryanbosire@gmail.com>
2022-08-18 15:31:33 +0300

    commit tree a7d84b2bf42454595e2df66fc73be4dd64d030ff

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    # .new gets initialized with a title
+    def initialize(title)
+        @title = title
+    end
 
-
+    # properties
+    attr_accessor :title, :author, :page_count, :genre
+    # turn_page
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+class Shoe
+# .new gets initialized with a brand
+    def initialize(brand)
+        @brand = brand
+    end
+# properties
+    attr_accessor :brand, :color, :material, :size, :condition
+    # cobble
+    def cobble
+        puts "Your shoe is as good as new!"
+        # makes the shoe's condition new
+        @condition = "new"
+    end
+end
